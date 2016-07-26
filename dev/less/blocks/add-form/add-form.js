de0d596c53b063623be8c2448084cb002d4777ca
NerV <voldemar.krs@gmail.com>
2016-07-26 15:32:40 +0700

    move vendors js
    to js/vendors dir

---
+++ dev/less/blocks/add-form/add-form.js
@@ -0,0 +1,9 @@
+function add_code(code, id) {
+    var textarea = document.getElementById(id);
+    var len = textarea.value.length,
+        start = textarea.selectionStart,
+        end = textarea.selectionEnd,
+        sel = textarea.value.substring(start, end),
+        replace = '['+code+']' + sel + '[/'+code+']';
+    textarea.value = textarea.value.substring(0,start) + replace + textarea.value.substring(end,len);
+}
